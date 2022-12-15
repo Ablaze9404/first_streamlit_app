0f2dad7af64a9a7527adfc55b9a744ae59bf9852
Ablaze9404 <120662965+Ablaze9404@users.noreply.github.com>
2022-12-15 12:45:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Oatmeal and Blueberries')
 streamlit.text('🥗 Bread Omlette')
 streamlit.text('🐔 Boiled Egg and Spinach')
-steeamlit.text('🥑🍞 Avocado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
